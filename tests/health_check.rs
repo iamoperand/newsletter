96d48eb2b3bef93764763143befd13fdc3370f7f
nikhil arora <narora200@gmail.com>
2022-06-14 18:35:17 +0530

    refactor: modularize codebase

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,7 @@
 use std::net::TcpListener;
 
+use newsletter::startup::run;
+
 #[tokio::test]
 async fn health_check_works() {
     let address = spawn_app();
@@ -65,7 +67,7 @@ fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("failed to bind random port.");
 
     let port = listener.local_addr().unwrap().port();
-    let server = newsletter::run(listener).expect("failed to bind address.");
+    let server = run(listener).expect("failed to bind address.");
 
     // launch the server as a background task
     // tokio::spawn returns a handle to the spawned future
