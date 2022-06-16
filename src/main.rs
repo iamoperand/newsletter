317b1e6f5dd75fa7f08b7cc7de21864b4e88c240
nikhil arora <narora200@gmail.com>
2022-06-16 12:17:47 +0530

    feat: enable telemetry via logs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use env_logger::Env;
 use sqlx::PgPool;
 use std::net::TcpListener;
 
@@ -5,6 +6,8 @@ use newsletter::{configuration::get_configuration, startup::run};
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let configuration = get_configuration().expect("failed to read configuration.");
     let connection_pool = PgPool::connect(&configuration.database.connection_string())
         .await
